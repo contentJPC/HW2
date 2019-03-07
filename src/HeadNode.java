865147c1e1f281d4bb4102baa13b4681cb47829b
tylerjmartinez <48234517+tylerjmartinez@users.noreply.github.com>
2019-03-07 03:02:29 -0800

    Update HeadNode.java

--- src/HeadNode.java
+++ src/HeadNode.java
@@ -1,6 +1,6 @@
 public interface HeadNode extends Node {
     HeadNode getNext();
     ValueNode getFirst();
-    void insert(ValueNode pValue);
-    int get(int pPosition);
+    void insert(ValueNode value);
+    int get(int position);
 }
