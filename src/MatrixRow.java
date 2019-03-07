344c8133742d8b31350e2b6f5cc0ea2b5d4cd15f
tylerjmartinez <48234517+tylerjmartinez@users.noreply.github.com>
2019-03-07 03:14:28 -0800

    Update MatrixRow.java

--- src/MatrixRow.java
+++ src/MatrixRow.java
@@ -1,4 +1,22 @@
-public class MatrixRow {
+public class MatrixRow implements HeadNode {
     private Node nextRow;
     private Node nextCol;
+    
+    public Node getNextRow() { return nextRow; }
+
+    public void setNextRow(Node next) { nextRow = next; }
+
+    public Node getNextCol() { return nextCol; }
+
+    public void setNextCol(Node next) { nextCol = next; }
+
+    public HeadNode getNext(){ return (HeadNode)nextRow; }
+
+    public ValueNode getFirst(){ return (ValueNode)nextCol; }
+    
+    void insert(ValueNode value);
+    
+    int get(int position){
+        return 0;
+    }
 }
