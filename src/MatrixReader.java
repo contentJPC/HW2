cda5de4ba31b533ccd3fbeb43dd5849cfb184d39
andrewSherry <46984390+andrewSherry@users.noreply.github.com>
2019-03-07 20:21:41 -0800

    Add files via upload

--- src/MatrixReader.java
+++ src/MatrixReader.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class MatrixReader {
     public SparseMatrix read(String pFile) {
-        return null;
+        int pRow = 0;
+        Scanner reader = new Scanner(pFile);
+        String line1 = reader.nextLine();
+        int numberOfRows = Integer.parseInt(line1);
+        String line2 = reader.nextLine();
+        int numberOfColumns = Integer.parseInt(line2);
+        SparseMatrix textFile = new SparseMatrix(numberOfRows, numberOfColumns);
+
+        while(reader.hasNext()) {
+            String currentLine =  reader.nextLine();
+            String[] tokens = currentLine.split(" ");
+            pRow = pRow + 1;
+            for (int j = 0; j < tokens.length; j++) {
+                String[] matrixValue = tokens[j].split(",");
+                int pCol = Integer.parseInt(matrixValue[0]);
+                int value = Integer.parseInt(matrixValue[1]);
+                textFile.insert(pRow, pCol, value);
+                
+            }
+        }
+
     }
 }
