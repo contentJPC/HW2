414fa5fef838b781c43459fa9ae16f2eaba2b28f
JPC <44622643+contentJPC@users.noreply.github.com>
2019-03-12 19:13:38 -0700

    Add files via upload

---
+++ MatrixColumn.java
@@ -0,0 +1,60 @@
+public class MatrixColumn implements HeadNode {
+    private ValueNode head = new ValueNode();
+    private Node nextRow;
+    private Node nextCol;
+
+    public boolean isEmpty(){return head == null;}
+
+    public Node getNextRow() { return nextRow; }
+
+    public void setNextRow(Node next) { nextRow = next; }
+
+    public Node getNextCol() { return nextCol; }
+
+    public void setNextCol(Node next) { nextCol = next; }
+
+    public HeadNode getNext(){ return (HeadNode)nextCol; }
+
+    public ValueNode getFirst(){ return (ValueNode) head.getNextRow(); }
+
+    public int get(int position){ //we want to return the value at a given point, for column position will be which row its in
+        //go to a specified position in the sparse matrix and return the value
+
+        ValueNode cur = head;
+        for(int i=0; i < position; i++) {
+            if (cur == null) {
+                //OH SHIT, THATS OUT OF THE MATRIX
+            }
+            cur = (ValueNode) cur.getNextRow();
+        }
+        return cur.getValue();
+    }
+
+    public void insert(ValueNode value) {
+        ValueNode cur = (ValueNode) head.getNextRow();
+        ValueNode preCur;
+        if(cur == null) {
+            head.setNextRow(value);
+        }
+        else if(cur.getRow() > value.getRow()) {
+            preCur = cur;
+            head.setNextRow(value);
+            value.setNextRow(preCur);
+        }
+        else if(cur.getRow() < value.getRow()) {
+            preCur = cur;
+            while (cur.getRow() < value.getRow()) {
+                try {
+                    cur = (ValueNode) cur.getNextRow();
+                    if (preCur.getRow() < value.getRow() && cur.getRow() > value.getRow()){
+                        preCur.setNextRow(value);
+                        value.setNextRow(cur);
+                    }
+                }
+                catch(NullPointerException e) {
+                    preCur.setNextRow(value);
+                }
+            }
+        }
+    }
+    }
---
+++ MatrixRow.java
@@ -0,0 +1,58 @@
+public class MatrixRow implements HeadNode {
+    private ValueNode head = new ValueNode();
+    private Node nextRow;
+    private Node nextCol;
+
+    public boolean isEmpty(){return head == null;}
+
+    public Node getNextRow(){return nextRow;}
+
+    public void setNextRow(Node next){nextRow = next;}
+
+    public Node getNextCol() {return nextCol;}
+
+    public void setNextCol(Node next) {nextCol = next;}
+
+    public HeadNode getNext(){return (HeadNode) nextRow;}
+
+    public ValueNode getFirst(){return (ValueNode) head.getNextCol();}
+
+    public int get (int position){ //we want to return the value at a given point, for row position will be which column its in
+        ValueNode cur = head;
+        for(int i=0; i < position; i++) {
+            if (cur == null) {
+                //OH SHIT, THATS OUT OF THE MATRIX
+            }
+            cur = (ValueNode) cur.getNextCol();
+        }
+        return cur.getValue();
+    }
+
+    public void insert(ValueNode value) {
+        ValueNode cur = (ValueNode) head.getNextCol();
+        ValueNode preCur;
+         if(cur == null) {
+             head.setNextCol(value);
+         }
+         else if(cur.getCol() > value.getCol()) {
+             preCur = cur;
+             head.setNextCol(value);
+             value.setNextCol(preCur);
+         }
+         else if(cur.getCol() < value.getCol()) {
+             preCur = cur;
+             while (cur.getCol() < value.getCol()) {
+                 try {
+                     cur = (ValueNode) cur.getNextCol();
+                     if (preCur.getCol() < value.getCol() && cur.getCol() > value.getCol()){
+                         preCur.setNextCol(value);
+                         value.setNextCol(cur);
+                     }
+                 }
+                 catch(NullPointerException e) {
+                     preCur.setNextCol(value);
+                 }
+             }
+         }
+         }
+    }
